0d017f5d14681a3d444257917e01d4c6d80d1afa
Corentin <corentin.42bonnet.cb@gmail.com>
2022-01-18 15:57:03 +0100

    Add routes last localisation

--- APP/LOCALISATION_API/routes.py
+++ APP/LOCALISATION_API/routes.py
@@ -2,7 +2,7 @@ from flask import request, jsonify
 import json
 
 from APP.LOCALISATION_API import bp_localisation
-from APP.LOCALISATION_API.services import get_localisation_by_user_and_time
+from APP.LOCALISATION_API.services import get_localisation_by_user_and_time, get_last_localisation_by_user
 
 
 @bp_localisation.route('/get_localisation_on_time', methods=('GET',))
@@ -10,3 +10,10 @@ def get_localisation():
     localisation = get_localisation_by_user_and_time()
 
     return jsonify([i.serialize for i in localisation])
+
+
+@bp_localisation.route('/get_last_localisation', methods=('GET',))
+def get_last_localisation():
+    localisation = get_last_localisation_by_user()
+
+    return jsonify(localisation.serialize)
--- APP/LOCALISATION_API/services.py
+++ APP/LOCALISATION_API/services.py
@@ -6,3 +6,9 @@ def get_localisation_by_user_and_time():
     r = Localisation.query.all()
 
     return r
+
+
+def get_last_localisation_by_user():
+    r = Localisation.query.order_by(Localisation.id.desc()).first()
+
+    return r
